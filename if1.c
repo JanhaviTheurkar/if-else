02c2f0ceb72ae199013eccbc703dc08ef1aa210c
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-09-19 21:11:20 +0530

    Add files via upload

---
+++ if1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main() {
+    int age;
+
+    printf("Enter age:");
+    scanf("%d",&age);
+
+    if (age>=18)
+    {
+        printf("You are Adult \n");
+    }
+    
+    else{
+        printf("You are not adult \n");
+    }
+
+    printf("Thank you \n");
+
+    return 0;
+
+}
+ 
+
